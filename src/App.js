cbf7d859748df27d0500ca61c690da255cb6304d
skqjatn6788@gmail.com <123361606+pass98@users.noreply.github.com>
2024-05-16 01:49:08 +0900

    css완료, 기능 디버깅중
    1. done인 상태에서 working으로 가지 못하는 경우
    2. working쪽의 데이터값을 인식하지 못하는 경우

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import styled from 'styled-components';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
   // state 상태창 관리
-  const [todos,setTodos] = useState([])
+  const [todos,setTodos] = useState([
+    { id:0, title:'코딩하기', todo:'투두리스트 만들기', isDone:false },
+    { id:1, title:'운동하기', todo:'러닝머신 뛰고오기', isDone:true },
+    { id:2, title:'책읽기', todo:'자기진로교재 한권읽기', isDone:false },
+  ])
   const [title,setTitle] = useState('');
   const [todo,setTodo] = useState('');
-  const [isDone,setIsdone] = useState(false)
+  const [isDone,setisDone] = useState(false)
   
   // 변화 감지 이벤트핸들러 
   const titleChangehandler = (event) => {
@@ -23,10 +28,12 @@ function App() {
       id : todos.length+1,
       title : title, 
       todo : todo,
-      isDone : true
-    }
-      setTodos([...todos, newTodos ])
-  }
+      isDone : isDone 
+    };
+      setTodos([...todos, newTodos ]);
+      setTitle('');
+      setTodo('');
+  };
   
   // 취소버튼
   const deletebutton = (id) => {
@@ -35,9 +42,33 @@ function App() {
     setTodos(newTodos)
   }
 
-  // isDone 체크
-  
+  // isDone 변환
+  const isDoneChangeHandler = (id) => {
+    alert(id)
+    console.log('해당 isDone은?',id.isDone)
+    const newTodo = todos.map(item => 
+      item.id === id ? { ...item, isDone:!item.isDone } : item
+    );
+    setTodos(newTodo);
+}
+
+  // 버튼 Css
+  const StBtn = styled.div`
+      width:100px;
+      height: 30px;
+      border: 0.4px solid green;
+      color: black;
+      font-weight: 200;
+      border-radius: 10px;
+      display: inline-block;
+      margin: auto;
+      text-align: center;
+      position: relative;
+      padding: 4.5px ;
+      `;
+
   return (
+    <div className='layout'>
     <div className="Mainpage">
       <span>MY TodoList</span>
       <div className='Header'>
@@ -52,26 +83,58 @@ function App() {
             value={todo}
             onChange={todoChangehandler}></input>
           <div className='addbuttonsection'>
-          <button className='Ip_Button' onClick={Addbuttonhandler}>추가</button>
+          <StBtn className='Ip_Button' onClick={Addbuttonhandler}>추가하기</StBtn>
           </div>
         </div>   
       </div>
-      <div className='TodoListItems'>
+      <div className='TodoListContainer'>
         <div className='TodoWorkingList'>
+        <div>
           Working!
-          {todos.map((item) => {
+          </div>
+          <div className='WorkingListsection'>
+          {todos.filter((item)=> !item.isDone).map((item) => {
           return  <div key={item.id} className='TodosItem'>
-            {item.title}  {item.todo}
-            <button onClick={() => deletebutton(item.id)}>x</button>
+          <div className='todoItemSection'>
+            <div className='todoTitle'>
+            {item.title}  
+            </div>
+            <div className='todocontent'>
+            {item.todo}
+            </div>
+            <div className='Buttonsection'>
+            <StBtn onClick={() => deletebutton(item.id)}>삭제하기</StBtn >
+            <StBtn onClick={() => isDoneChangeHandler(item.id)}>{item.isDone? '취소' : '완료'}</StBtn >
+            </div>
                   </div>
+            </div>
             })}
         </div>
+        </div>
         <div className='TododoneList'>
-          IsDone!
+          Done!
+          {todos.filter((item)=> item.isDone).map((item) => {
+          return  <div key={item.id} className='TodosItem'>
+          <div className='todoItemSection'>
+            <div className='todoTitle'>
+            {item.title}  
+            </div>
+            <div className='todocontent'>
+            {item.todo}
+            </div>
+            <div className='Buttonsection'>
+            <StBtn onClick={() => deletebutton(item)}>삭제하기</StBtn >
+            <StBtn  onClick={() => isDoneChangeHandler(item)}>{item.isDone? '취소' : '완료'}</StBtn >
+            </div>
+                  </div>
+            </div>
+            })}
         </div>
       </div>
     </div>
+    </div>
   );
 }
+
 
 export default App;
---
+++ src/components/button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const button = () => {
+    const style = () => {
+        
+    }
+  return (
+    <div>button</div>
+  )
+}
+
+export default button
